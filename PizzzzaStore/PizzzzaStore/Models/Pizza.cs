aa5d9cf1f32bc9dfdf65e181cf2d4fa9fb3b08a1
Richard Lee <lee.richardm@gmail.com>
2020-11-03 11:13:36 -0800

    initial commit

---
+++ PizzzzaStore/PizzzzaStore/Models/Pizza.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PizzzzaStore.Models
+{
+    public partial class Pizza
+    {
+        public int Id { get; set; }
+        public string Zzzzaname { get; set; }
+        public string Toppings { get; set; }
+        public string Sauce { get; set; }
+        public string Dough { get; set; }
+        public string Price { get; set; }
+    }
+}
---
+++ PizzzzaStore/PizzzzaStore/Models/PizzzzaStoreContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace PizzzzaStore.Models
+{
+    public partial class PizzzzaStoreContext : DbContext
+    {
+        public PizzzzaStoreContext()
+        {
+        }
+
+        public PizzzzaStoreContext(DbContextOptions<PizzzzaStoreContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Pizza> Pizza { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=LAPTOP-BA11OFN9;Database=PizzzzaStore;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pizza>(entity =>
+            {
+                entity.Property(e => e.Dough)
+                    .HasMaxLength(120)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Price)
+                    .HasMaxLength(120)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Sauce)
+                    .HasMaxLength(120)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Toppings)
+                    .HasMaxLength(120)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Zzzzaname)
+                    .HasColumnName("ZZZZAName")
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
