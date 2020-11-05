9b42058096510646f38804a6aabfaa2dc39af331
Richard Lee <lee.richardm@gmail.com>
2020-11-05 08:25:06 -0800

    changed connection string

--- PizzzzaStore/PizzzzaStore/Models/PizzzzaStoreContext.cs
+++ PizzzzaStore/PizzzzaStore/Models/PizzzzaStoreContext.cs
@@ -22,7 +22,7 @@ namespace PizzzzaStore.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=LAPTOP-BA11OFN9;Database=PizzzzaStore;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=tcp:pizzastoreserver.database.windows.net,1433;Initial Catalog=PizzzzaStore;Persist Security Info=False;User ID=pizza;Password=!Q@W3e4r;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
